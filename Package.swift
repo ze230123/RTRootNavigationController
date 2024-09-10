0ddae3d93c08ec9bca4f8d6d66966258324af03b
泽i <zhangzequn@eagersoft.cn>
2024-09-10 16:28:55 +0800

    添加Swift Package Manager支持

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RTRootNavigationController",
+    products: [
+            .library(name: "RTRootNavigationController", targets: ["RTRootNavigationController"]),
+        ],
+        dependencies: [],
+        targets: [
+            .target(
+                name: "RTRootNavigationController",
+                dependencies: [],
+                path: "RTRootNavigationController",
+                publicHeadersPath: "include",
+                cSettings: [
+                    //Config header path
+                    .headerSearchPath("."),
+                ]
+            ),
+        ]
+)
